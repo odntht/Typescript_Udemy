7063e570facb44a4c2ac150ab2c3670cfc23fcbe
Odntht <odntht@gmail.com>
2021-04-12 20:03:18 -0300

    add pequeno validador de coordenadas

--- src/CustomMap.ts
+++ src/CustomMap.ts
@@ -1,3 +1,5 @@
+import faker from 'faker';
+
 interface Mappable {
   location: {
     lat: number;
@@ -22,10 +24,7 @@ export class CustomMap {
   addMarker(mappable: Mappable): void {
     const marker = new google.maps.Marker({
       map: this.googleMap,
-      position: {
-        lat: mappable.location.lat,
-        lng: mappable.location.lng,
-      },
+      position: this.validateCoords(),
     });
     marker.addListener('click', () => {
       const infoWindow = new google.maps.InfoWindow({
@@ -34,5 +33,28 @@ export class CustomMap {
 
       infoWindow.open(this.googleMap, marker);
     });
+  }
+
+  validateCoords() {
+    let lat = parseFloat(faker.address.latitude());
+    let lng = parseFloat(faker.address.longitude());
+    if (lat < -85 || lat > 85) {
+      if (lat > -85) {
+        lat = -85;
+      } else {
+        lat = 85;
+      }
+    }
+    if (lng < -180 || lng > 180) {
+      if (lng > -180) {
+        lng = -180;
+      } else {
+        lng = 180;
+      }
+    }
+    return {
+      lat,
+      lng,
+    };
   }
 }

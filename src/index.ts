f27a463001d39fe002b3c8388e6a4388e1331e94
Odntht <odntht@gmail.com>
2021-04-12 12:57:04 -0300

    section 9

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import { User } from './User';
+import { Company } from './Company';
+import { CustomMap } from './CustomMap';
+
+let user = new User();
+console.log(user);
+
+let company = new Company();
+console.log(company);
+
+new CustomMap('map');
